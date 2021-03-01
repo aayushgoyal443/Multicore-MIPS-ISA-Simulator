3a7754d1fcb4645109e7fd630e1702f2f82afaed
Shrey <pshrey795@gmail.com>
2021-03-01 13:27:04 +0530

    Merged for saving

--- Assignment2/code.asm
+++ Assignment2/code.asm
@@ -5,11 +5,7 @@
 
 main:
 
-<<<<<<< HEAD
     li $s0,0        #current size of stack
-=======
-    li $s0,0        #sum variable
->>>>>>> c9d6471fe5701978186cf6682c83f230652f9af3
 
     li $v0,8        #Taking string as input
     la $a0,buffer
@@ -17,7 +13,6 @@ main:
     syscall
     move $s1,$a0    # iterator 
 
-<<<<<<< HEAD
     #ASCII bounds
     li $s3,48   # 0
     li $s4,57   # 9
@@ -25,14 +20,11 @@ main:
     li $s6,43   # +
     li $s7,45   # -
 
-=======
->>>>>>> c9d6471fe5701978186cf6682c83f230652f9af3
 loop:
 
     lb $s2,($s1)
     beq $s2,$zero,loop_exit
 
-<<<<<<< HEAD
     beq $s2,$s5,multiply
     beq $s2,$s6,addition
     beq $s2,$s7,subtraction
@@ -91,26 +83,6 @@ subtraction:
     sub	$t3, $t2, $t1
     sw $t3,($sp)
     j loop
-=======
-    #ASCII bounds
-    li $t0,48   # 0
-    li $t1,57   # 9
-    li $t2,42   # *
-    li $t3,42   # +
-    li $t4,45   # -
-
-    addi $s2,$s2,1
-
-    li $v0,11
-    move $a0,$s2
-    syscall
-
-    addi $s0,$s0,1
-    addi $s1,$s1,1
-
-    j loop
-
->>>>>>> c9d6471fe5701978186cf6682c83f230652f9af3
 
 char_error:
 
@@ -121,7 +93,6 @@ char_error:
     li $v0,10
     syscall
 
-<<<<<<< HEAD
 exp_error:
 
     li $v0,4
@@ -138,12 +109,6 @@ loop_exit:
 
     li $v0,1
     lw $a0,($sp)
-=======
-loop_exit:
-
-    li $v0,1
-    move $a0,$s0
->>>>>>> c9d6471fe5701978186cf6682c83f230652f9af3
     syscall
 
     li $v0,10
@@ -151,9 +116,5 @@ loop_exit:
 
 .data
 buffer: .space 52
-<<<<<<< HEAD
 error: .asciiz "\nInvalid character\n"
 error2: .asciiz "\nInvalid expression\n"
-=======
-error: .asciiz "\nPlease enter a valid character\n"
->>>>>>> c9d6471fe5701978186cf6682c83f230652f9af3
