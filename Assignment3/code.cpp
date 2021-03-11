3d2577b5fd3d5396232faa02474acd8987c8b947
Shrey <pshrey795@gmail.com>
2021-03-12 01:42:22 +0530

    Added basic operations and handled output on console

--- Assignment3/code.cpp
+++ Assignment3/code.cpp
@@ -36,6 +36,33 @@ void fill(unordered_map<string,int> &registers){
 	registers.insert(make_pair("ra",0));
 }
 
+void fill2(unordered_map<string,int> &operations)
+{
+	operations.insert(make_pair("add",0));
+	operations.insert(make_pair("sub",0));
+	operations.insert(make_pair("mul",0));
+	operations.insert(make_pair("mflo",0));
+	operations.insert(make_pair("beq",0));
+	operations.insert(make_pair("bne",0));
+	operations.insert(make_pair("slt",0));
+	operations.insert(make_pair("j",0));
+	operations.insert(make_pair("li",0));
+	operations.insert(make_pair("sw",0));
+	operations.insert(make_pair("addi",0));
+}
+
+void process(vector<string> tokens,unordered_map<string,int> &registers,unordered_map<string,int> &operations){
+	int m=tokens.size();
+	if(m==4){
+		if(tokens[0]=="add"){
+			registers[tokens[1]]=registers[tokens[2]]+registers[tokens[3]];
+		}
+	}else{
+		registers[tokens[1]]=stoi(tokens[2]);
+	}
+	operations[tokens[0]]++;
+}
+
 int main(int argc, char** argv)
 {
 	string fileName = argv[1];
@@ -46,6 +73,7 @@ int main(int argc, char** argv)
     	unordered_map<string,int> registers;
 		unordered_map<string,int> operations;
     	fill(registers);
+		fill2(operations);
     
     	while(getline(myFile,line)){
     		instructions.push_back(line);
@@ -54,11 +82,20 @@ int main(int argc, char** argv)
     
     	for(int i=0;i<n;i++){
     		string currentLine = instructions[i];
-    	
-    		//process currentLine and update registers
+			istringstream f(currentLine);
+			vector<string> strings;
+			string c;
+
+			while(getline(f,c,' ')){
+				strings.push_back(c);
+			}
+
+			process(strings,registers,operations);
+
     	
     		clockCycles++;
     		printRegisters(registers);
+			printRegisters(operations);
     	}
 
     	cout<<"No. of clock cycles: "<<clockCycles<<endl;
