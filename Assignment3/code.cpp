08fc5055dce0f47a5252c9a9326fe5138bf0b5b4
Shrey <pshrey795@gmail.com>
2021-03-12 18:43:43 +0530

    Basic working code

--- Assignment3/code.cpp
+++ Assignment3/code.cpp
@@ -5,6 +5,8 @@ using namespace std;
 map<string,int> registers;
 map<string,int> operations;
 map<string,int> labels;
+map<string,string> variables;
+vector<string> instructions;
 int itr = 0;
 
 void printRegisters(){
@@ -65,20 +67,81 @@ void fillOpers()
 
 void process(vector<string> tokens){
 	int m=tokens.size();
-	if(m==4){
-		if(tokens[0]=="add"){
-			registers[tokens[1]]=registers[tokens[2]]+registers[tokens[3]];
-		}
-	}else{
-		registers[tokens[1]]=stoi(tokens[2]);
+	string s=tokens[0];
+	if(m>4){
+		cout<<"Error";
+		return;
 	}
-	operations[tokens[0]]++;
+	else{
+		if(m==4){
+			if(s=="add"){
+				registers[tokens[1]]=registers[tokens[2]]+registers[tokens[3]];
+			}else if(s=="sub"){
+				registers[tokens[1]]=registers[tokens[2]]-registers[tokens[3]];
+			}else if(s=="mul"){
+				int prod=registers[tokens[1]]*registers[tokens[2]];
+				string currentLine = instructions[++itr];
+				istringstream f(currentLine);
+				vector<string> strings;
+				string c;
+				while(getline(f,c,' ')){
+					strings.push_back(c);		
+				}
+				int n = strings.size();
+				if(n==2){
+					if(strings[0]=="mflo"){
+						registers[strings[1]]=prod;
+						operations["mflo"]++;
+					}
+				}
+			}else if(s=="beq"){
+				if(registers[tokens[1]]==registers[tokens[2]]){
+					itr = labels[tokens[3]];
+				}
+			}else if(s=="bne"){
+				if(registers[tokens[1]]!=registers[tokens[2]]){
+					itr = labels[tokens[3]];
+				}
+			}else if(s=="slt"){
+				if(tokens[2]<tokens[3]){
+					registers[tokens[1]]=1;
+				}else{
+					registers[tokens[1]]=0;
+				}
+			}else if(s=="addi"){
+				registers[tokens[1]]=registers[tokens[2]]+stoi(tokens[3]);
+			}else{
+				return; //error
+			}
+		}else if(m==3){
+			if(s=="lw"){
+				registers[tokens[1]]=stoi(variables[tokens[2]]);
+			}else if(s=="sw"){
+				return; //edit
+			}else if(s=="li"){
+				registers[tokens[1]]=stoi(tokens[2]);
+			}else{
+				return; //error
+			}
+		}else if(m==2){
+			if(s=="j"){
+				itr=labels[tokens[1]];
+			}else{
+				return; //error
+			}
+		}else{
+			//Do nothing
+		}
+		itr++;
+		if(m!=1){
+			operations[tokens[0]]++;
+		}
+	}
 }
 
 int main(int argc, char** argv)
 {
 	string fileName = argv[1];
-	vector<string> instructions;
 	ifstream myFile(fileName);
 	string line;
 	int clockCycles=0;
@@ -89,6 +152,21 @@ int main(int argc, char** argv)
 		instructions.push_back(line);
 	}
 	int n=instructions.size();
+
+	for(int i=0;i<n;i++){
+		string currentLine = instructions[i];
+		istringstream f(currentLine);
+		vector<string> strings;
+		string c;
+
+		while(getline(f,c,' ')){
+			strings.push_back(c);		
+		}
+
+		if(strings.size()==1){
+			labels[strings[0]]=i;
+		}
+	}
 
 	while(itr<n){
 		string currentLine = instructions[itr];
