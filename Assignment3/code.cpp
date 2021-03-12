9feafe8ee99122abb32dcaf6840c01bc83bf0252
Shrey <pshrey795@gmail.com>
2021-03-12 23:30:21 +0530

    Implemented extensive error handling

--- Assignment3/code.cpp
+++ Assignment3/code.cpp
@@ -10,17 +10,21 @@ vector<string> instructions;
 int itr = 0;
 int throwError = 0;
 
-string int_to_hex( int x){
-	string s ="";
-	while(x!=0){
-		int r= x%16;
-		if (r>=0 && )
-	}
+bool check_number(string str) {
+   	if(!isdigit(str[0])){
+	   	if(str[0]!='-' && str[0]!='+'){
+		   return false;
+	   	}
+   	}
+   	for (int i = 1;i < str.length(); i++)
+   		if (isdigit(str[i]) == false)
+      		return false;
+    return true;
 }
 
 void printRegisters(){
 	for (auto i: registers){
-		cout<<i.first<<": "<<i.second<<endl;
+		cout<<(i.first).substr(1)<<": "<<i.second<<endl;
 	}
 	cout<<"\n";
 }
@@ -33,30 +37,30 @@ void printOperations(){
 }
 
 void fillRegs(){
-	registers["r0"]=0;
-	registers["at"]=0;
-	registers["v0"]=0;
-	registers["v1"]=0;
+	registers["$r0"]=0;
+	registers["$at"]=0;
+	registers["$v0"]=0;
+	registers["$v1"]=0;
 	
 	string c;
 	int m;
 	for(int i=0;i<3;i++){
 		switch(i)
 		{
-			case 0: {c="a";m=4;break;}
-			case 1: {c="t";m=10;break;}
-			case 2: {c="s";m=9;break;}
+			case 0: {c="$a";m=4;break;}
+			case 1: {c="$t";m=10;break;}
+			case 2: {c="$s";m=9;break;}
 		}
 		for(int j=0;j<m;j++){
 			registers[c+to_string(j)]=0;
 		}
 	}
 	
-	registers["k0"]=0;
-	registers["k1"]=0;
-	registers["gp"]=0;
-	registers["sp"]=2147479548;
-	registers["ra"]=0;
+	registers["$k0"]=0;
+	registers["$k1"]=0;
+	registers["$gp"]=0;
+	registers["$sp"]=2147479548;
+	registers["$ra"]=0;
 }
 
 void fillOpers()
@@ -76,61 +80,119 @@ void fillOpers()
 
 void process(vector<string> tokens){
 	int m=tokens.size();
-	string s=tokens[0];
+	string s0=tokens[0];
 	if(m>4){
 		cout<<"Syntax Error\n";
 		throwError =1;
 		return;
 	}
 	else{
-		if(m==4){
-			if(s=="add"){
-				registers[tokens[1]]=registers[tokens[2]]+registers[tokens[3]];
-			}else if(s=="sub"){
-				registers[tokens[1]]=registers[tokens[2]]-registers[tokens[3]];
-			}else if(s=="mul"){
-				registers[tokens[1]]=registers[tokens[2]]*registers[tokens[3]];
-			}else if(s=="beq"){
-				if(registers[tokens[1]]==registers[tokens[2]]){
-					itr = labels[tokens[3]];
-				}
-			}else if(s=="bne"){
-				if(registers[tokens[1]]!=registers[tokens[2]]){
-					itr = labels[tokens[3]];
-				}
-			}else if(s=="slt"){
-				if(registers[tokens[2]]<registers[tokens[2]]){
-					registers[tokens[1]]=1;
+		if(operations.find(s0)!=operations.end()){
+			if(m==2){
+				string s1=tokens[1];
+				if(labels.find(s1)!=labels.end()){
+					if(s0=="j"){
+						itr=labels[s1];
+					}
 				}else{
-					registers[tokens[1]]=0;
+					cout<<"Invalid register"<<endl;
+					throwError=1;
+					return;
 				}
-			}else if(s=="addi"){
-				registers[tokens[1]]=registers[tokens[2]]+stoi(tokens[3]);
-			}else{
-				return; //error
+			}else if(m==3){
+				string s1=tokens[1];
+				string s2=tokens[2];
+				if(s0=="lw"){
+					registers[s1]=stoi(variables[s2]);
+				}else if(s0=="sw"){
+					return; 		 //edit
+				}else{
+					registers[s1]=stoi(s2);
+				}
+			}else if(m==4){
+				string s1=tokens[1];
+				string s2=tokens[2];
+				string s3=tokens[3];
+				if(registers.find(s1)!=registers.end() && registers.find(s2)!=registers.end()){
+					if(s0=="add"){
+						if(registers.find(s3)!=registers.end()){
+							registers[s1]=registers[s2]+registers[s3];
+						}else{
+							cout<<"Invalid register\n";
+							throwError=1;
+							return;
+						}
+					}else if(s0=="sub"){
+						if(registers.find(s3)!=registers.end()){
+							registers[s1]=registers[s2]-registers[s3];
+						}else{
+							cout<<"Invalid register\n";
+							throwError=1;
+							return;
+						}
+					}else if(s0=="mul"){
+						if(registers.find(s3)!=registers.end()){
+							registers[s1]=registers[s2]*registers[s3];
+						}else{
+							cout<<"Invalid register\n";
+							throwError=1;
+							return;
+						}
+					}else if(s0=="slt"){
+						if(registers.find(s3)!=registers.end()){
+							if(registers[s2]<registers[s3]){
+								registers[s1]=1;
+							}else{
+								registers[s1]=0;
+							}
+						}else{
+							cout<<"Invalid register\n";
+							throwError=1;
+							return;
+						}
+					}else if(s0=="addi"){
+						if(check_number(s3)){
+							registers[s1]=registers[s2]+stoi(s3);
+						}else{
+							cout<<"Immediate value is not an integer\n";
+							throwError=1;
+							return;
+						}
+					}else if(s0=="beq"){
+						if(registers[s1]==registers[s2]){
+							if(labels.find(s3)!=labels.end()){
+								itr=labels[s3];
+							}else{
+								cout<<"Invalid label\n";
+								throwError=1;
+								return;
+							}
+						}
+					}else if(s0=="bne"){
+						if(registers[s1]!=registers[s2]){
+							if(labels.find(s3)!=labels.end()){
+								itr=labels[s3];
+							}else{
+								cout<<"Invalid label\n";
+								throwError=1;
+								return;
+							}
+						}
+					}
+				}else{
+					cout<<"Invalid register\n";
+					throwError=1;
+					return;
+				}
 			}
-		}else if(m==3){
-			if(s=="lw"){
-				registers[tokens[1]]=stoi(variables[tokens[2]]);
-			}else if(s=="sw"){
-				return; //edit
-			}else if(s=="li"){
-				registers[tokens[1]]=stoi(tokens[2]);
-			}else{
-				return; //error
-			}
-		}else if(m==2){
-			if(s=="j"){
-				itr=labels[tokens[1]];
-			}else{
-				return; //error
+			itr++;
+			if(m!=1){
+				operations[s0]++;
 			}
 		}else{
-			//Do nothing
-		}
-		itr++;
-		if(m!=1){
-			operations[tokens[0]]++;
+			cout<<"Invalid Instruction"<<endl;
+			throwError=1;
+			return;
 		}
 	}
 }
@@ -177,6 +239,9 @@ int main(int argc, char** argv)
 		}
 
 		process(strings);
+		if(throwError==1){
+			return 0;
+		}
 		clockCycles++;
 		printRegisters();
 	}
